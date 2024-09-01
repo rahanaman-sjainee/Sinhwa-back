a314b883d90289978022e35e4f8cef722a3a3b68
Aman <gr0315@cau.ac.kr>
2024-09-01 21:08:27 +0900

    feat:login API

--- src/main/java/com/example/backend/Controller/MainController.java
+++ src/main/java/com/example/backend/Controller/MainController.java
@@ -5,6 +5,7 @@ import com.example.backend.Auth.AuthFailException;
 import com.example.backend.Auth.UserProfile;
 import com.example.backend.Firebase.FirebaseService;
 import com.example.backend.Auth.Twitter.TwitterService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +31,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -56,12 +58,23 @@ public class MainController {
     private final OAuth1Operations twitterAuth;
 
     @GetMapping("/")
-    public String aa() {
+    public String aa(HttpServletResponse response) {
 
         //template 속 html 로 리다이렉트
         return "test";
 
     }
+    @GetMapping("/test")
+    public void test(HttpServletResponse response) throws IOException {
+        Cookie cookie = new Cookie("test", "test");
+//        cookie.setHttpOnly(true); // XSS 공격 방지를 위해 HttpOnly 설정
+//        cookie.setSecure(true); // HTTPS를 사용하는 경우 Secure 플래그 설정
+//        cookie.setPath("/");
+        cookie.setMaxAge(3600); // 쿠키의 유효 기간 설정 (예: 1시간)
+        response.addCookie(cookie);
+        response.sendRedirect("/");
+    }
+
     @GetMapping("/api")
     public ResponseEntity<Map<String, Object>> index() {
 
@@ -72,6 +85,27 @@ public class MainController {
 
 
     }
+
+    @GetMapping("api/login-token")
+    public ResponseEntity<String> getLoginToken(HttpServletRequest request){
+        String name ="idToken";
+        Cookie[] cookies = request.getCookies();
+        String token = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("idToken".equals(cookie.getName())) {
+                    token = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if (token == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token not found");
+        }
+        return ResponseEntity.ok(token);
+    }
+
+
     @GetMapping("api/login/twittr")
     public ResponseEntity<Void> loginTwitter(HttpServletResponse response) throws IOException {
 //        ResponseEntity<String> res = new RestTemplate().getForEntity(twitterService.getRequestURL(), String.class);
@@ -93,21 +127,40 @@ public class MainController {
 
     @GetMapping("api/login/twitter/process")
     @ResponseBody
-    public ResponseEntity<Map<String, Object>> twitterprocess(@RequestParam(value = "oauth_token",required = false) String oauthToken, @RequestParam(value = "oauth_verifier",required = false) String oauthVerifier) throws AuthFailException, IOException {
+    public void twitterprocess(@RequestParam(value = "oauth_token",required = false) String oauthToken, @RequestParam(value = "oauth_verifier",required = false) String oauthVerifier, HttpServletResponse response) throws AuthFailException, IOException {
         if(oauthToken == null || oauthVerifier == null){throw new AuthFailException();}
         OAuthToken accessToken = twitterAuth.exchangeForAccessToken(
                 new AuthorizedRequestToken(new OAuthToken(oauthToken, ""), oauthVerifier), null);
         Twitter twitter = new TwitterTemplate(consumerKey, consumerKeySecret, accessToken.getValue(), accessToken.getSecret());
         TwitterProfile profile = twitter.userOperations().getUserProfile();
-        String id = twitterService.getUserID(profile);
-        System.out.println(id);
-        Map<String, Object> response = new HashMap<>();
-        response.put("isSuccess", false);
-        response.put("id", id);
-        response.put("name", profile.getName());
-        return ResponseEntity.ok(response);
-        //response.sendRedirect("/");
+        Cookie cookie = new Cookie("idToken", twitterService.getUserID(profile));
+        cookie.setHttpOnly(true); // XSS 공격 방지를 위해 HttpOnly 설정
+//        cookie.setSecure(true); // HTTPS를 사용하는 경우 Secure 플래그 설정
+        cookie.setPath("/api");
+        cookie.setMaxAge(3600); // 쿠키의 유효 기간 설정 (예: 1시간)
+        response.addCookie(cookie);
+
+        // 프론트엔드 애플리케이션으로 리다이렉트
+       response.sendRedirect("http://192.168.219.101:3000/login");// 로그인 성공 페이지
+
+
     }
+
+//    public ResponseEntity<Map<String, Object>> twitterprocess(@RequestParam(value = "oauth_token",required = false) String oauthToken, @RequestParam(value = "oauth_verifier",required = false) String oauthVerifier) throws AuthFailException, IOException {
+//        if(oauthToken == null || oauthVerifier == null){throw new AuthFailException();}
+//        OAuthToken accessToken = twitterAuth.exchangeForAccessToken(
+//                new AuthorizedRequestToken(new OAuthToken(oauthToken, ""), oauthVerifier), null);
+//        Twitter twitter = new TwitterTemplate(consumerKey, consumerKeySecret, accessToken.getValue(), accessToken.getSecret());
+//        TwitterProfile profile = twitter.userOperations().getUserProfile();
+//        String id = twitterService.getUserID(profile);
+//        System.out.println(id);
+//        Map<String, Object> response = new HashMap<>();
+//        response.put("isSuccess", false);
+//        response.put("id", id);
+//        response.put("name", profile.getName());
+//        return ResponseEntity.ok(response);
+//        //response.sendRedirect("/");
+//    }
 
 //    public String twittertest(@RequestBody Map<String, Object> codeMap){
 //        TwitterConnectionFactory connectionFactory =
